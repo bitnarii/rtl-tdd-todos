7fdf36c53ec7aec8f5bdfdfb1ab3871516ee02dc
bitnarii <noreply_@hanmail.net>
2022-02-22 03:03:59 +0000

    TodoForm test element and event ok

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
-const TodoForm = () => {
+const TodoForm = ({onInsert}) => {
     const [value, setValue] = useState('');
     const onChange = useCallback(e => {
         setValue(e.target.value);
     }, []); //체인지이벤트가 일어날때 작업해야하는 내용 / [] 리프레시 호출 막기위해? 
+    const onSubmit = useCallback(e => {
+        onInsert(value);
+        setValue('');   //초기화
+        e.preventDefault(); //새로고침방치        
+    }, [onInsert, value]); //이 두개를 넘겨받는다
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <input placeholder = "할 일을 입력하세요~" value={value} onChange={onChange} />
-            <button type = "submit">등록</button>
+            <button type= "submit">등록</button>
         </form>
     );
 }
--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -3,7 +3,7 @@ import { fireEvent, render } from "@testing-library/react";
 import TodoForm from "./TodoForm";
 
 describe('<TodoForm/>', () => {
-    it('has input and a  buttuo', () =>{
+    it('has input and a  button', () =>{
         const {getByText, getByPlaceholderText} = render(<TodoForm/>); 
         getByPlaceholderText('할 일을 입력하세요~'); //input 있는지
         getByText('등록');     // 버튼 있는지 
@@ -12,7 +12,19 @@ describe('<TodoForm/>', () => {
         const {getByPlaceholderText} = render(<TodoForm/>);
         const input = getByPlaceholderText('할 일을 입력하세요~')
         fireEvent.change(input, {target: {value: 'TDD 배우기'}});  //value자리에 id, type 등 넣을 수 있음
-        expect(input).toHaveAttribute('value', 'TDD 배우기');
+        expect(input).toHaveAttribute('value', 'TDD 배우기');    
+    })
+    it('calls onInsert and clears input', () => {
+        const onInsert = jest.fn(); //브라우저에서 이벤트를 흉내내주는 역할을 함
+        const {getByText, getByPlaceholderText} = render(<TodoForm onInsert={onInsert} />); //props
+        const input = getByPlaceholderText('할 일을 입력하세요~');
+        const button = getByText('등록');
+        fireEvent.change(input, {target: {value: 'TDD 배우기'}}); 
+        fireEvent.click(button);
+        expect(onInsert).toBeCalledWith('TDD 배우기');
+        expect(input).toHaveAttribute('value', '')
+
+
     })
 
 
