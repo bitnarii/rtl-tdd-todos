9fde717e605bb2e738a328ec0d6db0a45bdbaca1
bitnarii <noreply_@hanmail.net>
2022-02-22 02:29:34 +0000

    TodoForm test nono

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoForm = () => {
+    const [value, setValue] = useState('');
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []); //체인지이벤트가 일어날때 작업해야하는 내용 / [] 리프레시 호출 막기위해? 
     return (
         <form>
-            <input placeholder = "할 일을 입력하세요~" />
-            <button type = 'submit'>등록</button>
+            <input placeholder = "할 일을 입력하세요~" value={value} onChange={onChange} />
+            <button type = "submit">등록</button>
         </form>
     );
 }
--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import TodoForm from "./TodoForm";
 
 describe('<TodoForm/>', () => {
     it('has input and a  buttuo', () =>{
-        const {getByText, getByPlaceholderText} = render(<TodoForm/>);
-        getByPlaceholderText('할 일을 입력하세요~'); 
-        getByText('등록'); 
-        
+        const {getByText, getByPlaceholderText} = render(<TodoForm/>); 
+        getByPlaceholderText('할 일을 입력하세요~'); //input 있는지
+        getByText('등록');     // 버튼 있는지 
     })
+    it('change input', () =>{
+        const {getByPlaceholderText} = render(<TodoForm/>);
+        const input = getByPlaceholderText('할 일을 입력하세요~')
+        fireEvent.change(input, {target: {value: 'TDD 배우기'}});  //value자리에 id, type 등 넣을 수 있음
+        expect(input).toHaveAttribute('value', 'TDD 배우기');
+    })
+
+
 })
