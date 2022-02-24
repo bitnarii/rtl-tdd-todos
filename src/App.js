81ca6f9787d3cbde57365f82801d860e02cf63c0
bitnarii <noreply_@hanmail.net>
2022-02-24 05:35:35 +0000

    auto deploy

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import TodoApp from './TodoApp'
 function App() {
   return (
     <div className="App">
+      <h2> CI/CD jenkis with githun auto deploy</h2>
       <TodoApp/>
-      <h2>하하하</h2>
     </div>
   );
 }
